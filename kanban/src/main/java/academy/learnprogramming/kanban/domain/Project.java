200c7949e9e045e138256b6945a0fa26a3e96d1e
Ross carmack <rosscarmack@rosss-mbp.home>
2020-03-31 11:57:49 -0500

    added validation to project

--- kanban/src/main/java/academy/learnprogramming/kanban/domain/Project.java
+++ kanban/src/main/java/academy/learnprogramming/kanban/domain/Project.java
@@ -1,9 +1,12 @@
 package academy.learnprogramming.kanban.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -13,13 +16,27 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter @Setter private Long id;
 
+    @NotBlank(message = "Project name is required")
     @Getter @Setter private String projectName;
+
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min=4, max=5, message="Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     @Getter @Setter private String projectIdentifier;
+
+    @NotBlank(message = "Project description is required")
     @Getter @Setter private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     @Getter @Setter private Date start_date;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     @Getter @Setter private Date end_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     @Getter @Setter private Date created_At;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     @Getter @Setter private Date updated_At;
 
     public Project() {
