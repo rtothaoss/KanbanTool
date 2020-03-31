a85010b8e7be53c88e4add351cbf969fb9507b39
Ross carmack <rosscarmack@rosss-mbp.home>
2020-03-31 13:46:58 -0500

    unique identifiers working

--- kanban/src/main/java/academy/learnprogramming/kanban/services/ProjectService.java
+++ kanban/src/main/java/academy/learnprogramming/kanban/services/ProjectService.java
@@ -1,6 +1,8 @@
 package academy.learnprogramming.kanban.services;
 
 import academy.learnprogramming.kanban.domain.Project;
+
+import academy.learnprogramming.kanban.exceptions.ProjectIdException;
 import academy.learnprogramming.kanban.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,12 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProjectService {
 
+
     @Autowired
     private ProjectRepository projectRepository;
 
+    public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
 
-    public Project saveOrUpdatePorject(Project project) {
-        return projectRepository.save(project);
     }
 
 }
