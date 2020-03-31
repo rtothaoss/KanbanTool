ac2fd0a48755b2949700abe8691408519f2e3135
Ross carmack <rosscarmack@rosss-mbp.home>
2020-03-31 12:40:18 -0500

    updated validation service

--- kanban/src/main/java/academy/learnprogramming/kanban/web/ProjectController.java
+++ kanban/src/main/java/academy/learnprogramming/kanban/web/ProjectController.java
@@ -2,16 +2,22 @@ package academy.learnprogramming.kanban.web;
 
 import academy.learnprogramming.kanban.domain.Project;
 import academy.learnprogramming.kanban.services.ProjectService;
+import academy.learnprogramming.kanban.services.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,11 +26,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private ValidationService validationService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-        if(result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
-        }
+
+        ResponseEntity<?> errorMap = validationService.MapValidationService(result);
+
+        if(errorMap != null) return errorMap;
+
 
         Project project1 = projectService.saveOrUpdatePorject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
