5588f8612280f3c60cdf5217ad65c1ef0cb22ea3
Ross carmack <rosscarmack@rosss-mbp.home>
2020-04-01 15:56:33 -0500

    react router added

--- kanban-react-client/src/App.js
+++ kanban-react-client/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
 import Header from './components/layout/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import AddProject from './components/project/AddProject';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path='/dashboard' component={Dashboard} />
+        <Route exact path='/addProject' component={AddProject} />
+      </div>
+    </Router>
   );
 }
 
