edbe1e213406493ee99105b9b2117d78cf129e22
Ross carmack <rosscarmack@rosss-mbp.home>
2020-04-11 16:51:57 -0500

    all CRUD operiations are tied to specific users

--- kanban/src/main/java/academy/learnprogramming/kanban/services/ProjectService.java
+++ kanban/src/main/java/academy/learnprogramming/kanban/services/ProjectService.java
@@ -29,6 +29,15 @@ public class ProjectService {
 
         String projectId = project.getProjectIdentifier().toUpperCase();
 
+        if(project.getId() != null) {
+            Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+            if(existingProject != null && (!existingProject.getProjectLeader().equals(username))){
+                throw new ProjectNotFoundException("Project not found in your account");
+            } else if(existingProject == null) {
+                throw new ProjectNotFoundException("Project with ID: '" + project.getProjectIdentifier() + "' cannot be updated because it does not exist.");
+            }
+        }
+
         try{
             User user = userRepository.findByUsername(username);
 
