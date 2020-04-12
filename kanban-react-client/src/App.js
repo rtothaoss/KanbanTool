7644f4c7b64d02d3dddf995b23f5f9d443392d9b
Ross carmack <rosscarmack@rosss-mbp.home>
2020-04-12 11:24:32 -0500

    added user registration with validation

--- kanban-react-client/src/App.js
+++ kanban-react-client/src/App.js
@@ -6,9 +6,12 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import AddProject from './components/project/AddProject';
 import UpdateProject from './components/project/UpdateProject';
-import ProjectBoard from './components/projectBoard/ProjectBoard'
-import AddProjectTask from './components/projectBoard/projectTasks/AddProjectTask'
-import UpdateProjectTask from './components/projectBoard/projectTasks/UpdateProjectTask'
+import ProjectBoard from './components/projectBoard/ProjectBoard';
+import AddProjectTask from './components/projectBoard/projectTasks/AddProjectTask';
+import UpdateProjectTask from './components/projectBoard/projectTasks/UpdateProjectTask';
+import Landing from './components/layout/Landing';
+import Register from './components/userManagement/Register';
+import Login from './components/userManagement/Login';
 import noMatch from './components/project/noMatch';
 
 function App() {
@@ -16,6 +19,13 @@ function App() {
     <Router>
       <div className="App">
         <Header />
+
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+
+
+
         <Route exact path='/dashboard' component={Dashboard} />
         <Route exact path='/addProject' component={AddProject} />
         <Route exact path='/updateProject/:id' component={UpdateProject} />
