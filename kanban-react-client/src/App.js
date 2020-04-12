48b78d674891607477a439cacc4bdb29c1535663
Ross carmack <rosscarmack@rosss-mbp.home>
2020-04-12 13:44:18 -0500

    Routes secured and React side finished

--- kanban-react-client/src/App.js
+++ kanban-react-client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
 import Header from './components/layout/Header';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import AddProject from './components/project/AddProject';
@@ -13,6 +13,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/userManagement/Register';
 import Login from './components/userManagement/Login';
 import noMatch from './components/project/noMatch';
+import SecuredRoute from './securityUtils/SecureRoute'
 
 
 function App() {
@@ -26,13 +27,14 @@ function App() {
         <Route exact path='/login' component={Login} />
 
 
-
-        <Route exact path='/dashboard' component={Dashboard} />
-        <Route exact path='/addProject' component={AddProject} />
-        <Route exact path='/updateProject/:id' component={UpdateProject} />
-        <Route exact path='/projectBoard/:id' component={ProjectBoard} />
-        <Route exact path='/addProjectTask/:id' component={AddProjectTask} />
-        <Route exact path='/updateProjectTask/:backlog_id/:pt_id' component={UpdateProjectTask} />
+      <Switch>
+        <SecuredRoute exact path='/dashboard' component={Dashboard} />
+        <SecuredRoute exact path='/addProject' component={AddProject} />
+        <SecuredRoute exact path='/updateProject/:id' component={UpdateProject} />
+        <SecuredRoute exact path='/projectBoard/:id' component={ProjectBoard} />
+        <SecuredRoute exact path='/addProjectTask/:id' component={AddProjectTask} />
+        <SecuredRoute exact path='/updateProjectTask/:backlog_id/:pt_id' component={UpdateProjectTask} />
+        </Switch>
       </div>
     </Router>
   );
